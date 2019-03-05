67276f54ea9eca32a63944401889b630ac3c4071
Bruno Calixto da Silva <b.calixto28@gmail.com>
2019-03-05 18:30:04 -0300

    (front-user-list): inserindo filtro por nome na lista de usuarios

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -12,14 +12,34 @@ export class UsersComponent implements OnInit {
   title: string = 'Usuários'
   errorMessage: string
   users: IUser[] = []
+  filteredUsers: IUser[]
+  _listFilter: string = ''
+  
+  get listFilter(): string {
+    return this._listFilter
+  }
+  
+  set listFilter(value: string) {
+    this._listFilter = value
+    this.filteredUsers = this.listFilter ? this.performFilter(this.listFilter) : this.users
+  }
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.userService.getUsers().subscribe(
-      users => this.users = users,
+      users => {
+        this.users = users
+        this.filteredUsers = this.users
+      },
       error => this.errorMessage = <any>error
     )
+  }
+
+  performFilter(filterBy: string): IUser[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.users.filter((user: IUser) =>
+      user.name.toLocaleLowerCase().indexOf(filterBy) !== -1)
   }
 
 }
