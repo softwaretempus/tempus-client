52d1c1528006158d87e3f24f594b181cfe5808d9
Bruno Sobral <bruno.sobral.silva@gmail.com>
2019-05-16 21:42:26 -0300

    primeira tentativa de utilizar componente schedule

--- src/app/views/agendamento/agendamento.component.ts
+++ src/app/views/agendamento/agendamento.component.ts
@@ -1,36 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-
+import { extend } from '@syncfusion/ej2-base'; 
+import { ChangeEventArgs } from '@syncfusion/ej2-buttons';
+import { ScheduleComponent, DragAndDropService, TimelineViewsService, GroupModel, EventSettingsModel, ResizeService, View } from '@syncfusion/ej2-angular-schedule';
+import { roomData } from './data';
+/*
+import { L10n, loadCldr} from '@syncfusion/ej2-base';
+import * as numberingSystems from './numberingSystems.json';
+import * as gregorian from './ca-gregorian.json';
+import * as numbers from './numbers.json';
+import * as timeZoneNames from './timeZoneNames.json';
+loadCldr(numberingSystems, gregorian, numbers, timeZoneNames);
+L10n.load({
+  'pt': {
+      'schedule': {
+          'day': 'Dia',
+          'week': 'Semana',
+          'workWeek': 'Semana de Trabalho',
+          'month': 'Mês',
+      }
+  }
+});
+*/
 @Component({
   templateUrl: 'agendamento.component.html',
-  styleUrls: ['agendamento.component.css']
+  styleUrls: ['agendamento.component.css'],
+  providers: [TimelineViewsService, ResizeService, DragAndDropService]
 })
 export class AgendamentoComponent {
 
   title: string = 'Agendamento'
   errorMessage: string;
 
-  columnDefs = [
-    {headerName: 'Analistas', field: 'analista', pinned: 'left' },    
-    {headerName: '15/05/2019' , field: '20190515', resizable: true },
-    {headerName: '16/05/2019', field: '20190516', resizable: true },
-    {headerName: '17/05/2019', field: '20190517', resizable: true },
-    {headerName: '18/05/2019', field: '20190518', resizable: true },
-    {headerName: '19/05/2019', field: '20190519', resizable: true },              
-    
+  public scheduleObj: ScheduleComponent;
+  public selectedDate: Date = new Date(2018, 7, 1);
+  public rowAutoHeight: Boolean = true;
+  public currentView: View = 'TimelineWeek';
+  public group: GroupModel = {
+    enableCompactView: false,
+    resources: ['MeetingRoom']
+  };
+  public allowMultiple: Boolean = true;
+  public resourceDataSource: Object[] = [
+    { text: 'Bruno Sobral', id: 1, color: '#98AFC7' },
+    { text: 'Gilvanleno', id: 2, color: '#99c68e' },
+    { text: 'Bruno Calixto', id: 3, color: '#C2B280' },
+    { text: 'Fabio Maia', id: 4, color: '#3090C7' },
+    { text: 'Juliana Lira', id: 5, color: '#95b9' },    
   ];
 
-  //cellStyle: {'border-left': '1px solid #b8b8b8'}
-
-  rowData = [
-      { analista: 'Bruno Sobral'   , 20190518: 'Embratel'  , 20190515: 'CEG', 20190516: 'Light' },
-      { analista: 'Fagner Oliveira', 20190515: 'Petrobras' },
-      { analista: 'Bruno Calixto'  , 20190518: 'Coca-Cola' },
-      { analista: 'Gilvanleno Mota', 20190515: 'Ambev'     },
-      { analista: 'Juliana Lira'   , 20190518: 'Riomix'    },
-      { analista: 'Fabio Silva'    , 20190515: 'Light'     },
-  ];
+  public eventSettings: EventSettingsModel = {
+    dataSource: <Object[]>extend([], roomData, null, true),
+    fields: {
+      id: 'Id',
+      subject: { name: 'Subject', title: 'Summary' },
+      location: { name: 'Location', title: 'Location' },
+      description: { name: 'Description', title: 'Comments' },
+      startTime: { name: 'StartTime', title: 'From' },
+      endTime: { name: 'EndTime', title: 'To' }
+    }
+  };
   
   constructor(
     private route: ActivatedRoute,
@@ -38,6 +68,10 @@ export class AgendamentoComponent {
     private toastr: ToastrService
   ) { 
 
+  }
+
+  onChange(args: ChangeEventArgs): void {
+    this.scheduleObj.rowAutoHeight = args.checked;
   }
 
 }
--- src/app/views/agendamento/agendamento.module.ts
+++ src/app/views/agendamento/agendamento.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
-import { AgGridModule } from 'ag-grid-angular';
 
 import { AgendamentoComponent } from './agendamento.component';
 import { AgendamentoRoutingModule } from './agendamento-routing.module';
+import { ScheduleAllModule, RecurrenceEditorAllModule } from '@syncfusion/ej2-angular-schedule';
 
 @NgModule({
   imports: [
     AgendamentoRoutingModule,
     ReactiveFormsModule,
     SharedModule,
-    AgGridModule.withComponents([])
+    ScheduleAllModule,
+    ScheduleAllModule    
   ],
   declarations: [
     AgendamentoComponent,    
